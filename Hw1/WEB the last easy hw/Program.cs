5c5a0624d769ffd7ecace6c7e988e357bad6f223
Anna <anuta.denisovna@gmail.com>
2019-11-01 20:29:08 +1000

    Hello world

---
+++ Hw1/WEB the last easy hw/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEB_the_last_easy_hw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
