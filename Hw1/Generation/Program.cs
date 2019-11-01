f798b793f1df9e5210095d51d9f748e5741aa2e2
Anna <anuta.denisovna@gmail.com>
2019-11-01 20:43:50 +1000

    Generation

---
+++ Hw1/Generation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Generation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of array");
+            int a = Convert.ToInt16(Console.ReadLine());
+            int[] array = new int[a];
+            Console.WriteLine("Enter min number");
+            int minimum = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter max number");
+            int maximum = Convert.ToInt16(Console.ReadLine());
+            Random rnd = new Random();
+
+            for (int i = 0; i < a; i++)
+            {
+                array[i] = rnd.Next(minimum, maximum);
+            }
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
