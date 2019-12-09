6ad4dd52f132fe38fa1ff937b6cb42acfa20e946
Anna <anuta.denisovna@gmail.com>
2019-12-09 22:48:54 +1000

    Task 3.5

---
+++ WEB_hw3/Task_Routing/ErrorHandlingMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Task_Routing
+{
+    public class ErrorHandlingMiddleware
+    {
+        private RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next.Invoke(context);
+            if (context.Response.StatusCode == 403)
+            {
+                await context.Response.WriteAsync("Access Denied");
+            }
+            else if (context.Response.StatusCode == 404)
+            {
+                await context.Response.WriteAsync("Not Found");
+            }
+        }
+    }
+}
