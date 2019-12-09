70a23c030573dae326b11bf4bb49bdf8bc8a8930
Anna <anuta.denisovna@gmail.com>
2019-12-10 09:19:13 +1000

    Task 3.5

---
+++ hw3/Task_Error_Routing_Auth/ErrorHandlingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Task_Error_Routing_Auth
+{
+    public class ErrorHandlingMiddleware
+    {
+        private RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next.Invoke(context);
+            switch (context.Response.StatusCode)
+            {
+                case 403:
+                    await context.Response.WriteAsync("Access Denied");
+                    break;
+                case 404:
+                    await context.Response.WriteAsync("Not Found");
+                    break;
+            }
+        }
+    }
+}
---
+++ hw3/Task_Error_Routing_Auth/RoutingMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Task_Error_Routing_Auth
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+ 
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var path = context.Request.Path.Value;
+            switch (path)
+            {
+                case "/Task1":
+                    await context.Response.WriteAsync("Hello world");
+                    break;
+                case "/Task2":
+                    await context.Response.WriteAsync("Formula1");
+                    break;
+                case "/Task3":
+                    await context.Response.WriteAsync("Formula2");
+                    break;
+                case "/Task4":
+                    await context.Response.WriteAsync("Formula3");
+                    break;
+                default:
+                    context.Response.StatusCode = 404;
+                    break;
+            }
+
+            //await _next.Invoke(context);
+        } 
+    }
+}
