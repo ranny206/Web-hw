45c833d3b83feed3d1c878d003b7c92f8bb0f30b
Anna <anuta.denisovna@gmail.com>
2019-12-10 09:18:03 +1000

    Task 3.1-3.4

---
+++ hw3/Task1/MyMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Task1
+{
+    public class MyMiddleware
+    {
+        private readonly RequestDelegate _next;
+ 
+        public MyMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+ 
+        public async Task InvokeAsync(HttpContext context)
+        {
+            int angle = 30;
+            double doubleAngle = Math.Pow(Math.Cos(angle), 2) - Math.Pow(Math.Sin(angle), 2);
+            await context.Response.WriteAsync($"cos a = {doubleAngle}");
+        }
+    }
+}
---
+++ hw3/Task1/UseMyMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Task1
+{
+    public static class UseMyMiddleware
+    {
+        public static IApplicationBuilder UseMyMw(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMiddleware>();
+        }
+    }
+}
